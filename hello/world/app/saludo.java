c986e3747a3c7073e124fff5a87a38427eac7991
Rubén Carrera Amoedo <rcarreraamoedo@victoria11.ad.danielcastelao.org>
2019-10-04 14:06:11 +0200

    Inicio

---
+++ hello/world/app/saludo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.app;
+
+/**
+ *
+ * @author rcarreraamoedo
+ */
+public class saludo {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+}
+}
